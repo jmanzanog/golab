c3cac40222ab98d2479ecf7833400ce689d36d91
jose.manzano <j.manzano.g@hotmail.com>
2020-09-07 12:52:09 -0500

    Logrus example

--- http/main.go
+++ http/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	server := NewServer(":4400")
 	server.Handle("/", "GET", server.AddMiddleware(HandleRoot, Logging(), CheckAuth()))
-	server.Handle("/home", "GET", HandleHome)
+	server.Handle("/home", "GET", server.AddMiddleware(HandleHome, Logging()))
 	server.Handle("/create", "POST", PostRequest)
 	server.Handle("/user", "POST", server.AddMiddleware(CreateRequest, CheckCreateRequest()))
 	server.Listen()
